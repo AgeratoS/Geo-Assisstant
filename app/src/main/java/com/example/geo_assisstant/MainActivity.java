18474d589c0bb9c2b49f6d53f02c01db0124d958
Alexey <leshka99514@gmail.com>
2019-04-02 22:33:00 +0300

    The map has created successfully

--- app/src/main/java/com/example/geo_assisstant/MainActivity.java
+++ app/src/main/java/com/example/geo_assisstant/MainActivity.java
@@ -3,11 +3,43 @@ package com.example.geo_assisstant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
 
+public class MainActivity extends AppCompatActivity {
+    private MapView _mapview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey("6510675d-95b1-40ed-9948-689720a64118");
+        MapKitFactory.initialize(this);
         setContentView(R.layout.activity_main);
+
+        _mapview = (MapView)findViewById(R.id.mapview);
+        _mapview.getMap().move(
+                new CameraPosition(new Point(55.751574, 37.573856), 11.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 0),
+                null);
     }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        _mapview.onStop();
+        MapKitFactory.getInstance().onStop();
+    }
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+        _mapview.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+
+
 }
